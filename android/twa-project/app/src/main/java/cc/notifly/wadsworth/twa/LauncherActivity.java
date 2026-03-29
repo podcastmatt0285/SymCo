f6c5af4831a83e6c648f64bace939261e6681890
podcastmatt0285 <podcastmatt0285@gmail.com>
2026-03-28 18:48:18 -0700

    db

--- android/twa-project/app/src/main/java/cc/notifly/wadsworth/twa/LauncherActivity.java
+++ android/twa-project/app/src/main/java/cc/notifly/wadsworth/twa/LauncherActivity.java
@@ -22,21 +22,12 @@ import android.os.Bundle;
 import android.provider.Settings;
 import java.security.MessageDigest;
 
-
-
 public class LauncherActivity
         extends com.google.androidbrowserhelper.trusted.LauncherActivity {
-    
-
-    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Setting an orientation crashes the app due to the transparent background on Android 8.0
-        // Oreo and below. We only set the orientation on Oreo and above. This only affects the
-        // splash screen and Chrome will still respect the orientation.
-        // See https://github.com/GoogleChromeLabs/bubblewrap/issues/496 for details.
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         } else {
