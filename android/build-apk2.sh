581a3b67c9911ea2d8c7617fafd07cf08247ed16
podcastmatt0285 <podcastmatt0285@gmail.com>
2026-05-20 11:49:09 -0700

    local changes

--- android/build-apk2.sh
+++ android/build-apk2.sh
@@ -19,8 +19,8 @@ set -e
 DOMAIN="wadsworth.notifly.cc"
 APP_NAME="Wadsworth"
 PACKAGE="cc.notifly.wadsworth"           # ← change if yours differs
-VERSION_CODE=22
-VERSION_NAME="1.22"
+VERSION_CODE=23
+VERSION_NAME="2.01"
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 WIDGET_DIR="${SCRIPT_DIR}/widget"
